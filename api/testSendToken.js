f169603061171105d638f09954d87e849179e88e
matinkim8000 <yesican0070@gmail.com>
2025-12-05 10:38:54 +0900

    Update testSendToken.js

--- api/testSendToken.js
+++ api/testSendToken.js
@@ -1,16 +1,7 @@
-// /api/testSendToken.js
-
 export default function handler(req, res) {
-  try {
-    return res.status(200).json({
-      status: "success",
-      message: "Vercel API 정상 동작 중!",
-      timestamp: new Date().toISOString()
-    });
-  } catch (err) {
-    return res.status(500).json({
-      status: "error",
-      message: err.message
-    });
-  }
+  return res.status(200).json({
+    status: "success",
+    message: "Vercel API 정상 작동!",
+    time: new Date().toISOString()
+  });
 }
